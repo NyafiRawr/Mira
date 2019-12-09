b339ddb59d66b4699cb240b29ac66decf7c9cfb5
Kirill Kudryavtsev <deissh@yandex.ru>
2019-12-09 11:18:03 +0300

    fix(modules/osu): useless code and osmosis

--- src/modules/osu.js
+++ src/modules/osu.js
@@ -1,6 +1,5 @@
 /* eslint-disable quote-props */
 import axios from 'axios';
-import osmosis from 'osmosis';
 import users from './users';
 import config from '../config';
 import tools from './tools';
@@ -34,7 +33,7 @@ function getKeyOnValue(nameFileData, value) {
   throw new Error();
 }
 
-module.exports.getValueOnKey = (nameFileData, key) => {
+export const getValueOnKey = (nameFileData, key) => {
   const data = getData(nameFileData);
   if (key in data) {
     return data[key];
@@ -42,45 +41,7 @@ module.exports.getValueOnKey = (nameFileData, key) => {
   return null;
 };
 
-function akatsukiHTMLConvertToData(html) {
-  return osmosis // todo: различие режимов (читает только первый 0)
-    .get(html)
-    .set({ 'username': 'meta[property="og:title"]@content' })
-    .set({ 'user_id': 'body > div.ui.full.height.main.wrapper > div.h-container > div:nth-child(2) > script' })
-    .set({ 'count300': null })
-    .set({ 'count100': null })
-    .set({ 'count50': null })
-    .set({ 'playcount': 'body > div.ui.full.height.main.wrapper > div.h-container > div:nth-child(2) > div:nth-child(20) > div > div > div:nth-child(3) > div:nth-child(1) > table > tbody > tr:nth-child(5) > td.right.aligned' })
-    .set({ 'ranked_score': 'body > div.ui.full.height.main.wrapper > div.h-container > div:nth-child(2) > div:nth-child(20) > div > div > div:nth-child(3) > div:nth-child(1) > table > tbody > tr:nth-child(3) > td.right.aligned' })
-    .set({ 'total_score': 'body > div.ui.full.height.main.wrapper > div.h-container > div:nth-child(2) > div:nth-child(20) > div > div > div:nth-child(3) > div:nth-child(1) > table > tbody > tr:nth-child(4) > td.right.aligned' })
-    .set({ 'pp_rank': 'body > div.ui.full.height.main.wrapper > div.h-container > div:nth-child(2) > div:nth-child(20) > div > div > div:nth-child(3) > div:nth-child(1) > table > tbody > tr:nth-child(1) > td.right.aligned' }) //0,1,2,3? - unknow! danger
-    .set({ 'level': 'body > div.ui.full.height.main.wrapper > div.h-container > div:nth-child(2) > div:nth-child(20) > div > div > div:nth-child(3) > div:nth-child(1) > div > div.label' })
-    .set({ 'pp_raw': 'body > div.ui.full.height.main.wrapper > div.h-container > div:nth-child(2) > div:nth-child(20) > div > div > div:nth-child(3) > div:nth-child(1) > table > tbody > tr:nth-child(2) > td.right.aligned' })
-    .set({ 'accuracy': 'body > div.ui.full.height.main.wrapper > div.h-container > div:nth-child(2) > div:nth-child(20) > div > div > div:nth-child(3) > div:nth-child(1) > table > tbody > tr:nth-child(10) > td.right.aligned' })
-    .set({ 'count_rank_ss': null })
-    .set({ 'count_rank_ssh': null })
-    .set({ 'count_rank_s': null })
-    .set({ 'count_rank_sh': null })
-    .set({ 'count_rank_a': null })
-    .set({ 'country': 'body > div.ui.full.height.main.wrapper > div.h-container > div:nth-child(2) > div:nth-child(20) > div > div > div:nth-child(1) > b:nth-child(2)' })
-    .set({ 'total_seconds_played': 'body > div.ui.full.height.main.wrapper > div.h-container > div:nth-child(2) > div:nth-child(20) > div > div > div:nth-child(3) > div:nth-child(1) > table > tbody > tr:nth-child(7) > td.right.aligned' })
-    .set({ 'pp_country_rank': null })
-    // Уникальные для сервера строки:
-    .set({ 'playtime': 'body > div.ui.full.height.main.wrapper > div.h-container > div:nth-child(2) > div:nth-child(20) > div > div > div:nth-child(3) > div:nth-child(1) > table > tbody > tr:nth-child(6) > td.right.aligned' })
-    .set({ 'registration_date': 'body > div.ui.full.height.main.wrapper > div.h-container > div:nth-child(2) > div:nth-child(20) > div > div > div:nth-child(1) > time:nth-child(4)' })
-    .set({ 'last_seen': 'body > div.ui.full.height.main.wrapper > div.h-container > div:nth-child(2) > div:nth-child(20) > div > div > div:nth-child(1) > time:nth-child(6)' })
-    .set({ 'replay_watch': 'body > div.ui.full.height.main.wrapper > div.h-container > div:nth-child(2) > div:nth-child(20) > div > div > div:nth-child(3) > div:nth-child(1) > table > tbody > tr:nth-child(8) > td.right.aligned' })
-    .set({ 'total_hits': 'body > div.ui.full.height.main.wrapper > div.h-container > div:nth-child(2) > div:nth-child(20) > div > div > div:nth-child(3) > div:nth-child(1) > table > tbody > tr:nth-child(9) > td.right.aligned' })
-    .set({ 'followers': 'body > div.ui.full.height.main.wrapper > div.h-container > div:nth-child(2) > div:nth-child(20) > div > div > div:nth-child(3) > div:nth-child(1) > table > tbody > tr:nth-child(11) > td.right.aligned' })
-    .data(function (listing) {
-      // eslint-disable-next-line prefer-destructuring
-      listing.user_id = listing.user_id.split('window.')[2].split(' ')[3];
-      // todo: обработать данные (убрать ,# и тд)
-    })
-    .error(console.log);
-}
-
-module.exports.get_user = async (idOrName, mode = 0, server = 'ppy') => {
+export const get_user = async (idOrName, mode = 0, server = 'ppy') => {
   const result = await axios.get('/api/get_user', {
     baseURL: `http://${getKeyOnValue('server', server)}`,
     params: {
@@ -89,16 +50,13 @@ module.exports.get_user = async (idOrName, mode = 0, server = 'ppy') => {
       k: server === 'ppy' ? config.osu_token : undefined,
     },
     transformResponse: [function (data) {
-      if (server === 'akatsuki-relax') {
-        return akatsukiHTMLConvertToData(data);
-      }
       return JSON.parse(data)[0];
     }],
   });
   return result.data;
 };
 
-module.exports.get_user_recent = (idOrName, mode = 0, server = 'ppy') => {
+export const get_user_recent = (idOrName, mode = 0, server = 'ppy') => {
   let url = `https://${getKeyOnValue('server', server)}/api/get_user_recent?m=${mode}&u=${idOrName}&limit=${1}`;
 
   if (server === 'ppy') {
@@ -108,7 +66,7 @@ module.exports.get_user_recent = (idOrName, mode = 0, server = 'ppy') => {
   return axios.get(url);
 };
 
-module.exports.get_user_best = (idOrName, mode = 0, server = 'ppy', limit = 5) => {
+export const get_user_best = (idOrName, mode = 0, server = 'ppy', limit = 5) => {
   const limitMax = 10;
 
   let url = `https://${getKeyOnValue('server', server)}/api/get_user_best?m=${mode}&u=${idOrName}&limit=${limit > limitMax ? limitMax : limit}`;
@@ -120,7 +78,7 @@ module.exports.get_user_best = (idOrName, mode = 0, server = 'ppy', limit = 5) =
   return axios.get(url);
 };
 
-module.exports.get_scores = (idMap, idPlayer, mode = 0, server = 'ppy') => {
+export const get_scores = (idMap, idPlayer, mode = 0, server = 'ppy') => {
   let url = `https://${getKeyOnValue('server', server)}/api/get_scores?m=${mode}&b=${idMap}&u=${idPlayer}&limit=${1}`;
 
   if (server === 'ppy') {
@@ -130,7 +88,7 @@ module.exports.get_scores = (idMap, idPlayer, mode = 0, server = 'ppy') => {
   return axios.get(url);
 };
 
-module.exports.get_beatmap = (idMap, mode = 0, server = 'ppy') => {
+export const get_beatmap = (idMap, mode = 0, server = 'ppy') => {
   let url = `https://${getKeyOnValue('server', server)}/api/get_beatmaps?m=${mode}&b=${idMap}`;
 
   if (server === 'ppy') {
@@ -140,7 +98,7 @@ module.exports.get_beatmap = (idMap, mode = 0, server = 'ppy') => {
   return axios.get(url);
 };
 
-module.exports.convertLength = (length) => {
+export const convertLength = (length) => {
   const dt = new Date();
   dt.setTime(length * 1000);
   const seconds = dt.getUTCSeconds();
@@ -150,7 +108,7 @@ module.exports.convertLength = (length) => {
   return `${dt.getUTCMinutes()}:${seconds}`;
 };
 
-module.exports.convertDatetime = (datetime) => {
+export const convertDatetime = (datetime) => {
   const [date, time] = datetime.split(' ');
   const [year, month, day] = date.split('-');
   // eslint-disable-next-line no-unused-vars
@@ -158,7 +116,7 @@ module.exports.convertDatetime = (datetime) => {
   return `${day}.${month}.${year} ${hours}:${minutes}`;
 };
 
-module.exports.calculateAccuracity = (mode, count300, count100, count50,
+export const calculateAccuracity = (mode, count300, count100, count50,
   countmiss, countkatu, countgeki) => {
   let userScore;
   let totalScore;
@@ -208,7 +166,7 @@ module.exports.calculateAccuracity = (mode, count300, count100, count50,
   return null;
 };
 
-module.exports.showStatistic = (mode, count300, count100, count50,
+export const showStatistic = (mode, count300, count100, count50,
   countmiss, countkatu, countgeki) => {
   const reCount300 = tools.separateThousandth(count300);
   const reCountgeki = tools.separateThousandth(countgeki);
@@ -274,7 +232,7 @@ function findPlayer(user, message) {
   return { ...findedPlayer };
 }
 
-module.exports.searchPlayer = (message, args) => {
+export const searchPlayer = (message, args) => {
   const reArgs = args.join(' ');
   let player = {};
 
@@ -291,7 +249,7 @@ module.exports.searchPlayer = (message, args) => {
   return player;
 };
 
-module.exports.getModsFromJson = (code) => {
+export const getModsFromJson = (code) => {
   // eslint-disable-next-line import/no-webpack-loader-syntax
   const mods = require('./../data/osu!/mods.json');
   let reCode = parseInt(code, 10);
@@ -313,7 +271,7 @@ module.exports.getModsFromJson = (code) => {
   return result.join(', ');
 };
 
-module.exports.getKeyFromSearchOnValueFromJson = (filename, value) => {
+export const getKeyFromSearchOnValueFromJson = (filename, value) => {
   const list = require(`../data/osu!/${filename}.json`);
   let searchResult = false;
   let result;
@@ -342,7 +300,7 @@ module.exports.getKeyFromSearchOnValueFromJson = (filename, value) => {
   return { result, searchResult };
 };
 
-module.exports.getValueOnKeyFromJson = (filename, key) => {
+export const getValueOnKeyFromJson = (filename, key) => {
   const list = require(`../data/osu!/${filename}.json`);
 
   const result = list[key];
