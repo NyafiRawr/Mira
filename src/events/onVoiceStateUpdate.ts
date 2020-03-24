752ffb3c3431f87dd864f12895f24cfd03d2b695
NyafiRawr <leonid.b4@yandex.ru>
2020-03-24 22:45:58 +0700

    Фикс времени в войсе

--- src/events/onVoiceStateUpdate.ts
+++ src/events/onVoiceStateUpdate.ts
@@ -5,22 +5,17 @@ const inVoice = new Map();
 
 // Если юзер зашёл в войс фиксируем дату входа, если вышел вычисляем время в войсе и записываем в базу
 export default async (oldMember: GuildMember, newMember: GuildMember) => {
-  const oldUserChannel = oldMember.voiceChannel;
   const newUserChannel = newMember.voiceChannel;
 
-  if (oldUserChannel === undefined && newUserChannel !== undefined) {
+  if (!inVoice.has(`${newMember.guild.id}_${newMember.id}`) && newUserChannel !== undefined) {
     inVoice.set(`${newMember.guild.id}_${newMember.id}`, Date.now());
-  } else if (newUserChannel === undefined) {
-    const entryVoiceDate = inVoice.get(`${newMember.guild.id}_${newMember.id}`);
-    if (!entryVoiceDate) {
-      return;
-    } else {
-      inVoice.delete(`${newMember.guild.id}_${newMember.id}`);
-    }
-    const seconds = (Date.now() - entryVoiceDate) / 1000;
-    const user = await users.get(oldMember.guild.id, oldMember.id);
-    await users.set(oldMember.guild.id, oldMember.id, {
-      voiceTime: user?.voiceTime || 0 + seconds,
+  } else if (newUserChannel === undefined && inVoice.has(`${newMember.guild.id}_${newMember.id}`)) {
+    const seconds = (Date.now() - inVoice.get(`${newMember.guild.id}_${newMember.id}`)) / 1000;
+    inVoice.delete(`${newMember.guild.id}_${newMember.id}`);
+    const user = await users.get(newMember.guild.id, newMember.id);
+    const time = user?.voiceTime || 0;
+    await users.set(newMember.guild.id, newMember.id, {
+      voiceTime: time + seconds,
     });
   }
 };
