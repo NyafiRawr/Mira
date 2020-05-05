543d8940248435349fe348a854108daaec1baa50
NyafiRawr <leonid.b4@yandex.ru>
2020-05-06 01:21:41 +0700

    Система варнов

---
+++ src/commands/Модерация/warning.ts
@@ -0,0 +1,45 @@
+import * as Discord from 'discord.js';
+import CustomError from '../../utils/customError';
+import * as warns from '../../modules/warnings';
+
+module.exports = {
+  name: __filename.slice(__dirname.length + 1).split('.')[0],
+  description: 'Сделать предупреждение, чтобы не нарушал правил',
+  aliases: ['warn'],
+  usage: '@кому [почему]',
+  guild: true,
+  hide: true,
+  cooldown: 0.5,
+  cooldownMessage: undefined,
+  permissions: ['MANAGE_MESSAGES'],
+  group: __dirname.split(/[\\/]/)[__dirname.split(/[\\/]/).length - 1],
+  async execute(message: Discord.Message, args: string[]) {
+    if (!message.member.hasPermission(this.permissions[0])) {
+      throw new CustomError('нужно право управлять сообщениями!');
+    }
+
+    if (!message.guild.members.get(message.client.user.id)!.hasPermission(this.permissions[0])) {
+      throw new CustomError('у меня нет права управлять сообщениями!');
+    }
+
+    const victim = message.mentions.members.first();
+    if (!victim)
+      throw new CustomError(
+        'пожалуйста, укажите кому нужно сделать предупреждение при вызове команды!'
+      );
+
+    let reason = args.slice(1).join(' ');
+    if (!reason.trim().length) reason = 'Не указана';
+
+    await warns.set(message.guild.id, victim.id, reason);
+    return message.channel.send({
+      'embed': {
+        'author': {
+          'name': `${victim} получает предупреждение`,
+          'icon_url': victim.user.avatarURL
+        },
+        'description': `**Причина:** ${reason}`,
+      }
+    });
+  },
+};
