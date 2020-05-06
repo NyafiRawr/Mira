bb4b93a50007428e0e09f781002a619748353bfd
NyafiRawr <leonid.b4@yandex.ru>
2020-05-06 22:26:02 +0700

    Свадьба и развод

---
+++ src/commands/Общение/mme.ts
@@ -0,0 +1,36 @@
+import * as Discord from 'discord.js';
+import CustomError from '../../utils/customError';
+import * as users from '../../modules/users';
+import * as menu from '../../utils/menu';
+
+module.exports = {
+  name: __filename.slice(__dirname.length + 1).split('.')[0],
+  description: 'Свадьба',
+  aliases: ['marryme', 'marry'],
+  usage: '@с_кем',
+  guild: true,
+  hide: false,
+  cooldown: 3,
+  cooldownMessage: undefined,
+  permissions: undefined,
+  group: __dirname.split(/[\\/]/)[__dirname.split(/[\\/]/).length - 1],
+  async execute(message: Discord.Message) {
+    if (!message.mentions.members.size) throw new CustomError('необходимо упомянуть того на ком хотите жениться!');
+    const victim = message.mentions.members.first();
+    if (victim.id === message.author.id) throw new CustomError('нельзя жениться на самом себе!');
+
+    const me = await users.get(message.guild.id, message.author.id);
+    if (!!me?.coupleId) throw new CustomError('гаремы не разрешены!');
+
+    message.channel.send(`**${victim} будь со мной!** - говорит ${message.author}, если согласна(-ен) напиши: \`ДА\`, иначе отвергни!`);
+    const answer = await menu.waitMessage(message.channel, victim.id);
+    if (!answer || answer.toLowerCase() !== 'да') {
+      return message.reply(`время ответа на предложение вышло или тебя отвергли...`);
+    }
+    else {
+      await users.set(message.guild.id, victim.id, { coupleId: message.author.id });
+      await users.set(message.guild.id, message.author.id, { coupleId: victim.id });
+      return message.channel.send(`**${victim} и ${message.author} поженились!**`);
+    }
+  },
+};
---
+++ src/commands/Общение/umme.ts
@@ -0,0 +1,31 @@
+import * as Discord from 'discord.js';
+import CustomError from '../../utils/customError';
+import * as users from '../../modules/users';
+
+module.exports = {
+  name: __filename.slice(__dirname.length + 1).split('.')[0],
+  description: 'Развод',
+  aliases: ['unmarry'],
+  usage: '@с_кем',
+  guild: true,
+  hide: false,
+  cooldown: 3000,
+  cooldownMessage: undefined,
+  permissions: undefined,
+  group: __dirname.split(/[\\/]/)[__dirname.split(/[\\/]/).length - 1],
+  async execute(message: Discord.Message) {
+    const me = await users.get(message.guild.id, message.author.id);
+    if (!me?.coupleId) throw new CustomError('у тебя никого нет, чтобы разводится!');
+
+    const victim = message.guild.members.get(me.coupleId);
+    if (!victim) {
+      await users.set(message.guild.id, me.coupleId, { coupleId: null });
+      await users.set(message.guild.id, message.author.id, { coupleId: null });
+      return message.reply('похоже, что ваша пара сбежала с сервера, вы разведены.');
+    }
+
+    await users.set(message.guild.id, victim.id, { coupleId: message.author.id });
+    await users.set(message.guild.id, message.author.id, { coupleId: victim.id });
+    return message.channel.send(`**${message.author} РАЗВЕЛСЯ С ${victim}.**`);
+  },
+};
