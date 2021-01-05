591a1cab8e1d6964b30820350d87245e5620416c
NyafiRawr <leonid.b4@yandex.ru>
2021-01-06 00:52:34 +0700

    Восстановил доступность и поправил ссылки

--- src/commands/Разработка/donate.ts
+++ src/commands/Разработка/donate.ts
@@ -1,4 +1,4 @@
-import { Message, MessageEmbed } from 'discord.js';
+import { Message } from 'discord.js';
 import config from '../../config';
 
 module.exports = {
@@ -7,34 +7,55 @@ module.exports = {
   aliases: ['donut'],
   group: __dirname.split(/[\\/]/)[__dirname.split(/[\\/]/).length - 1],
   execute(message: Message) {
-    message.channel.send(
-      new MessageEmbed()
-        .setColor(config.colors.donate)
-        .setDescription(
+    message.channel.send({
+      embed: {
+        color: config.colors.donate,
+        description:
           '❤️ В качестве благодарности за поддержку: 1 рубль = 69:cookie:\n' +
-            '*На все сервера, где есть я, даже, если тебя там нет!*'
-        )
-        .addField(
-          'ЯД/Юmoney',
-          '[410014841265118](https://money.yandex.ru/to/410014841265118)',
-          true
-        )
-        .addField(
-          'QIWI',
-          '[Копилка](https://qiwi.me/81e51b22-d3bf-4ec4-b72f-5d4f537abc1b)',
-          true
-        )
-        .addField(
-          'WebMoney',
-          '[WMR: R119858043867](https://www.webmoney.ru/rus/inout/topup.shtml#bank-card_method)',
-          true
-        )
-        .setImage(
-          'http://img.nga.178.com/attachments/mon_201911/01/-64xyuQ5-a59uXsZ7pT3cShs-a0.gif'
-        )
-        .setFooter(
-          '«Возможности не приходят сами — вы создаете их» © Крис Гроссер'
-        )
-    );
+          '*На все сервера, где есть я, даже, если тебя там нет!*',
+        fields: [
+          {
+            name: 'ЯД/Юmoney',
+            value:
+              '[410014841265118](https://money.yandex.ru/to/410014841265118)',
+            inline: true,
+          },
+          {
+            name: 'WebMoney',
+            value:
+              '[WMR: R119858043867](https://www.webmoney.ru/rus/inout/topup.shtml#bank-card_method)',
+            inline: true,
+          },
+          {
+            name: 'Ник на QIWI',
+            value: '[NYAFIRAWR](https://qiwi.com/payment/form/99999)',
+            inline: true,
+          },
+          {
+            name: 'PayPal',
+            value: '[Nyafiru](https://www.paypal.me/nyafiru)',
+            inline: true,
+          },
+          {
+            name: '-',
+            value: '-',
+            inline: true,
+          },
+          {
+            name: '-',
+            value: '-',
+            inline: true,
+          },
+        ],
+        image: {
+          url:
+            'http://img.nga.178.com/attachments/mon_201911/01/-64xyuQ5-a59uXsZ7pT3cShs-a0.gif',
+        },
+        footer: {
+          text:
+            '«Возможности не приходят сами — вы создаете их» © Крис Гроссер',
+        },
+      },
+    });
   },
 };
