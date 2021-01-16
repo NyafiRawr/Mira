7c2d78751e74810d83c8d4b63df7cd428a94340b
NyafiRawr <leonid.b4@yandex.ru>
2021-01-17 03:31:48 +0700

    Обновление взаимодействия

--- src/commands/Игры/guess.ts
+++ src/commands/Игры/guess.ts
@@ -1,44 +1,69 @@
-import { Message, MessageEmbed } from 'discord.js';
-import { randomBoolean } from '../../utils';
+import { Collection, Message, MessageEmbed } from 'discord.js';
+import { randomInteger } from '../../utils';
 import * as economy from '../../modules/economy';
 import config from '../../config';
 
-const awardCookies = 100;
-
+const awardCookies = 50;
+const timeoutSeconds = 15; // Секунд
 const body = {
   color: config.colors.message,
   author: {
-    name: 'Загадываю цифру ...',
+    name: 'Загадываю цифру от одного до трёх ...',
+  },
+  title: 'Попробуй угадать!',
+  footer: {
+    text: `Время на ответ: ${timeoutSeconds}`,
   },
 };
 
 module.exports = {
   name: __filename.slice(__dirname.length + 1).split('.')[0],
-  description: 'Угадай цифру',
-  usage: '<цифра от 1 до 10>',
+  description: 'Угадай цифру и получи :cookie:',
   cooldown: {
     seconds: 75600,
-    messages: ['загадываю новую цифру (timeLeft)'],
+    messages: ['придумываю новую цифру (timeLeft)'],
   },
   group: __dirname.split(/[\\/]/)[__dirname.split(/[\\/]/).length - 1],
-  async execute(message: Message, args: string[]) {
-    const amount = parseInt(args[0], 10);
-    if (!Number.isInteger(amount) || amount < 1) {
-      throw new Error(
-        'ты не указал цифру, она должна быть целочисленной и положительной.'
-      );
-    }
-
-    const correct = randomBoolean(); // Выше шанс угадать, чем реально загадывать цифру
+  async execute(message: Message) {
+    const num = randomInteger(1, 3);
 
     const embed = new MessageEmbed(body);
-    if (correct) {
-      economy.setBalance(message.guild!.id, message.author.id, awardCookies);
-    }
-    message.channel.send(
-      embed
-        .setTitle(correct ? 'ВЕРНО' : 'НЕ ПОВЕЗЛО')
-        .setDescription(correct ? ':gift:' : ':negative_squared_cross_mark:')
-    );
+    const msg = await message.channel.send(embed);
+
+    const filter = (messageCollection: Message) =>
+      message.author.id === messageCollection.author.id;
+
+    await message.channel
+      .awaitMessages(filter, {
+        max: 1,
+        time: timeoutSeconds * 1000,
+        errors: ['time'],
+      })
+      .then(async (collected: Collection<string, Message>) => {
+        if (collected.first()?.content === num.toString()) {
+          await msg.edit(
+            embed
+              .addField(
+                `Верно, это ${num}!`,
+                `В награду ты получаешь **+${awardCookies}**:cookie:`
+              )
+              .setFooter('')
+          );
+          economy.setBalance(
+            message.guild!.id,
+            message.author.id,
+            awardCookies
+          );
+        } else {
+          await msg.edit(
+            embed
+              .addField(`Не верно, это было ${num}!`, `Повезёт в следующий раз`)
+              .setFooter('')
+          );
+        }
+      })
+      .catch(async () => {
+        await msg.edit(embed.setFooter(`Время вышло! Я загадывала ${num}.`));
+      });
   },
 };
