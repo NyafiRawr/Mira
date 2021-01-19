7d2a536373b1da04d2b7163b9ee2cf8c1b4c8a5f
NyafiRawr <leonid.b4@yandex.ru>
2021-01-20 00:18:44 +0700

    Проверка доступа к команде

--- src/commands/Игры/Лотерея/maxmembers.ts
+++ src/commands/Игры/Лотерея/maxmembers.ts
@@ -2,7 +2,13 @@ import { Message } from 'discord.js';
 import * as vars from '../../../modules/vars';
 import { keyMaxMembers } from '../lottery';
 
+const permission = 'ADMINISTRATOR';
+
 export const maxmembers = async (message: Message, args: string[]) => {
+  if (!message.member?.hasPermission(permission)) {
+    throw new Error(`нужно иметь глобальную привилегию: ${permission}.`);
+  }
+
   const num = args.shift();
   if (num === undefined) {
     throw new Error('не указано новое число участников.');
