349e5b14420f57b166a4d1fe0eb0220db3ea335e
NyafiRawr <leonid.b4@yandex.ru>
2021-01-20 23:09:13 +0700

    Более быстрая версия

--- src/commands/Модерация/purge.ts
+++ src/commands/Модерация/purge.ts
@@ -5,11 +5,15 @@ module.exports = {
   name: __filename.slice(__dirname.length + 1).split('.')[0],
   description: 'Удаление сообщений',
   aliases: ['remove', 'clear', 'prune', 'delete'],
-  usage: '[@писатель] <кол-во>',
+  usage: '<кол-во> [@писатель]',
+  cooldown: {
+    seconds: 3,
+  },
   permissions: ['MANAGE_MESSAGES'],
   group: __dirname.split(/[\\/]/)[__dirname.split(/[\\/]/).length - 1],
   async execute(message: Message, args: string[]) {
     const channel = message.guild!.channels.resolve(message.channel.id);
+
     if (!channel?.permissionsFor(message.member!)?.has(this.permissions[0])) {
       throw new Error('у тебя нет права управлять сообщениями в этом канале!');
     }
@@ -19,32 +23,45 @@ module.exports = {
       throw new Error('у меня нет права управлять сообщениями в этом канале!');
     }
 
-    let amount: number;
-    const member = message.mentions.members?.first();
-    if (member == undefined) {
-      amount = parseInt(args[0], 10);
-    } else {
-      amount = parseInt(args[1], 10);
+    const arg = args.shift();
+    if (arg === undefined) {
+      throw new Error('не указано количество удаляемых сообщений.');
     }
+
+    const amount = parseInt(arg, 10);
     if (isInteger(amount) == false) {
-      throw new Error('не указано количество.');
+      throw new Error('количество должно быть целочисленным и положительным.');
     }
-    // Удаление больше 100 может вызвать непредсказуемое поведение (заметка по API 2018 года)
-    if (amount < 1 || amount > 100) {
-      throw new Error('можно удалить только от 1 до 100 сообщений за раз.');
-    }
-
-    let messages = await message.channel.messages.fetch({ limit: amount });
-    if (member) {
-      messages = messages.filter((msg) => msg.author.id == member.id);
+    if (amount < 1 && amount > 100) {
+      throw new Error(
+        'минимум 1 и не более 100 сообщений для удаления за раз.'
+      );
     }
 
-    messages.map(async (msg) => await msg.delete());
+    if (message.mentions.users.size === 0 && message.channel.type === 'text') {
+      const messages = await message.channel.bulkDelete(amount, true);
+      await (
+        await message.channel.send(
+          `Удалено сообщений: **${messages.size}**, ${message.author}\nЭтот отчёт скоро исчезнет :alarm_clock:`
+        )
+      ).delete({ timeout: 3000 });
+    } else {
+      let messages = await message.channel.messages.fetch({ limit: amount });
 
-    message.channel
-      .send(
-        `Удаление **${messages.size}** сообщений(я), ${message.author}\nЗавершение через несколько секунд :alarm_clock:`
-      )
-      .then(async (msg) => await msg.delete({ timeout: 3000 }));
+      const user = message.mentions.users.first();
+      if (user) {
+        messages = messages.filter((msg) => msg.author.id == user.id);
+      }
+
+      const reportMessage = await message.channel.send(
+        `Удаление **${messages.size}** сообщений(я), ${message.author}\nЭто сообщение будет удалено по заврешению :alarm_clock:`
+      );
+
+      for await (const [, msg] of messages) {
+        await msg.delete({ reason: 'Purge' });
+      }
+
+      await reportMessage.delete({ timeout: 1000 });
+    }
   },
 };
