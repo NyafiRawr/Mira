bf196afc51840acfa2a0cb0ef0811c3546da4590
NyafiRawr <leonid.b4@yandex.ru>
2021-01-21 21:55:17 +0700

    Сообщение о том что участвуешь в лотерее, без join

--- src/commands/Игры/Лотерея/info.ts
+++ src/commands/Игры/Лотерея/info.ts
@@ -1,4 +1,4 @@
-import { Message } from 'discord.js';
+import { Message, MessageEmbed } from 'discord.js';
 import config from '../../../config';
 import { separateThousandth } from '../../../utils';
 import { lots } from '../lot';
@@ -6,31 +6,28 @@ import { lots } from '../lot';
 export const info = async (message: Message) => {
   const lottery = lots.get(message.guild!.id);
 
-  let embed;
+  const embed = new MessageEmbed({
+    color: config.games.lottery.color,
+    author: {
+      name: 'Лотерея',
+    },
+  });
   if (lottery === undefined) {
-    embed = {
-      color: config.games.lottery.color,
-      author: {
-        name: 'Лотерея',
-      },
-      title: 'Ничего не проводится',
-    };
+    embed.setTitle('Ничего не проводится');
   } else {
-    embed = {
-      color: config.games.lottery.color,
-      author: {
-        name: 'Лотерея',
-      },
-      title: `Розыгрыш: ${separateThousandth(
-        lottery.prize.toString()
-      )}:cookie:`,
-      description:
+    embed
+      .setTitle(
+        `Розыгрыш: ${separateThousandth(lottery.prize.toString())}:cookie:`
+      )
+      .setDescription(
         `Организатор: <@${lottery.authorId}>` +
-        `\nУчастники: ${lottery.members.length}/${lottery.membersMaxCount}`,
-      footer: {
-        text: `Участвовать: ${config.discord.prefix}lottery join`,
-      },
-    };
+          `\nУчастники: ${lottery.members.length}/${lottery.membersMaxCount}`
+      );
+    if (lottery.members.some((id) => message.author.id === id)) {
+      embed.setFooter('Ты принял участие в этой лотерее');
+    } else {
+      embed.setFooter(`Участвовать: ${config.discord.prefix}lottery join`);
+    }
   }
 
   await message.channel.send({ embed });
